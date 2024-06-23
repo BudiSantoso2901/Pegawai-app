55b030a3f72a09a56400f05f903deef7fa5c308c
Budi Santoso <budi.santoso290103@gmail.com>
2024-06-23 19:27:31 +0700

    tambah library

--- resources/views/Admin/view.blade.php
+++ resources/views/Admin/view.blade.php
@@ -128,6 +128,7 @@
 @endsection
 @push('js')
     <script src="//cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+    <script src="//cdn.datatables.net/1.10.25/js/dataTables.bootstrap5.min.js"></script>
 @endpush
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
